0d34fa1fd197ffc4982e6aab2e7200b8e53543e9
Kang Hen <hendriarga28@gmail.com>
2022-04-18 21:40:49 +0700

    Cek SQl dan Insert

--- excel_hibah2022.php
+++ excel_hibah2022.php
@@ -18,14 +18,11 @@ $request = new Request;
 $data = new Spreadsheet_Excel_Reader($_FILES['impor']['name'],false);
 // menghitung jumlah baris data yang ada
 $jumlah_baris = $data->rowcount($sheet_index=0);
-
-var_dump($data); // Cek dahulu apakah data di excel terbaca, biasakan untuk membaca data terlebih dahulu sebelum melakukan saving
+ // Cek dahulu apakah data di excel terbaca, biasakan untuk membaca data terlebih dahulu sebelum melakukan saving
 // jumlah default data yang berhasil di import
 
 $jenis_pengajuan="hibah";
 for ($i=2; $i<=$jumlah_baris; $i++){
-
-	
 	$jenis_pengajuan     = $data->val($i, 1);
 	$kecamatan     = $data->val($i, 2);
 	$desa     = $data->val($i, 3);
@@ -62,7 +59,18 @@ for ($i=2; $i<=$jumlah_baris; $i++){
 		/**
 		 * Coba check tulisan pada query ini created_at harusnya $created_at
 		 */
-		$query=mysqli_query($koneksi,"INSERT into hibah_2022 ('peruntukan,tahun_pengajuan,kecamatan,desa,program,kegiatan,sub_kegiatan,opd_rekomendasi,opd_pelaksana,uraian_keg_satuan,penerima,pimpinan,bhi,alamat,nominal,tahun_terakhir_menerima,tanggal_permohonan,nomor_permohonan,nomor_penerbitan_rekomendasi,pejabat_penerbitan_rekomendasi,tanggal_penerbitan_rekomendasi,tanggal_disposisi_bupati,tanggal_pertimbangan_ketua_tapd,isi_disposisi_ketua_tapd','created_at','created_by') values('$peruntukan','$tahun_pengajuan', '$kecamatan','$desa','program','$kegiatan','$sub_kegiatan','$opd_rekomendasi','$opd_pelaksana', $uraian_keg_satuan','$penerima','$pimpinan','$bhi','$alamat','$nominal','$tahun_terakhir_menerima','$tanggal_permohonan','$nomor_permohonan','$nomor_penerbitan_rekomendasi','$pejabat_penerbitan_rekomendasi','$tanggal_penerbitan_rekomendasi','$tanggal_disposisi_bupati','$tanggal_pertimbangan_ketua_tapd','$isi_disposisi_ketua_tapd','created_at','$created_by')");
+		$sql = "INSERT into hibah_2022 ('peruntukan,tahun_pengajuan,kecamatan,desa,program,kegiatan,sub_kegiatan,opd_rekomendasi,opd_pelaksana,uraian_keg_satuan,penerima,pimpinan,bhi,alamat,nominal,tahun_terakhir_menerima,tanggal_permohonan,nomor_permohonan,nomor_penerbitan_rekomendasi,pejabat_penerbitan_rekomendasi,tanggal_penerbitan_rekomendasi,tanggal_disposisi_bupati,tanggal_pertimbangan_ketua_tapd,isi_disposisi_ketua_tapd','created_at','created_by') values('$peruntukan','$tahun_pengajuan', '$kecamatan','$desa','program','$kegiatan','$sub_kegiatan','$opd_rekomendasi','$opd_pelaksana', $uraian_keg_satuan','$penerima','$pimpinan','$bhi','$alamat','$nominal','$tahun_terakhir_menerima','$tanggal_permohonan','$nomor_permohonan','$nomor_penerbitan_rekomendasi','$pejabat_penerbitan_rekomendasi','$tanggal_penerbitan_rekomendasi','$tanggal_disposisi_bupati','$tanggal_pertimbangan_ketua_tapd','$isi_disposisi_ketua_tapd','$created_at','$created_by')";
+
+		var_dump($sql);
+		/**
+		 * Jika kode di atas hasilnya sudah benar
+		 */
+
+		 $save = $this->db->query($sql);
+
+		 var_dump($save);
+		
+		//$query=mysqli_query($koneksi,"INSERT into hibah_2022 ('peruntukan,tahun_pengajuan,kecamatan,desa,program,kegiatan,sub_kegiatan,opd_rekomendasi,opd_pelaksana,uraian_keg_satuan,penerima,pimpinan,bhi,alamat,nominal,tahun_terakhir_menerima,tanggal_permohonan,nomor_permohonan,nomor_penerbitan_rekomendasi,pejabat_penerbitan_rekomendasi,tanggal_penerbitan_rekomendasi,tanggal_disposisi_bupati,tanggal_pertimbangan_ketua_tapd,isi_disposisi_ketua_tapd','created_at','created_by') values('$peruntukan','$tahun_pengajuan', '$kecamatan','$desa','program','$kegiatan','$sub_kegiatan','$opd_rekomendasi','$opd_pelaksana', $uraian_keg_satuan','$penerima','$pimpinan','$bhi','$alamat','$nominal','$tahun_terakhir_menerima','$tanggal_permohonan','$nomor_permohonan','$nomor_penerbitan_rekomendasi','$pejabat_penerbitan_rekomendasi','$tanggal_penerbitan_rekomendasi','$tanggal_disposisi_bupati','$tanggal_pertimbangan_ketua_tapd','$isi_disposisi_ketua_tapd','$created_at','$created_by')");
 		
 		
 		/**
